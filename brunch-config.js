1afa6d784018e375539002c1c7de5c5990917b8f
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-26 22:40:36 +0100

    style(brunch-config.js): eslintify

--- brunch-config.js
+++ brunch-config.js
@@ -2,7 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      joinTo: 'js/app.js',
 
       // To use a separate vendor.js bundle, specify two files path
       // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
@@ -21,63 +21,65 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css',
     },
     templates: {
-      joinTo: "js/app.js"
-    }
+      joinTo: 'js/app.js',
+    },
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/web/static/assets". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(web\/static\/assets)/
+    assets: /^(web\/static\/assets)/,
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
     watched: [
-      "web/static",
-      "test/static"
+      'web/static',
+      'test/static',
     ],
 
     // Where to compile files to
-    public: "priv/static"
+    public: 'priv/static',
   },
 
   // Configure your plugins
   plugins: {
     sass: {
       options: {
-        includePaths: ["node_modules/bootstrap/scss"], // tell sass-brunch where to look for files to @import
-        precision: 8 // minimum precision required by bootstrap
-      }
+        // tell sass-brunch where to look for files to @import
+        includePaths: ['node_modules/bootstrap/scss'],
+        // minimum precision required by bootstrap
+        precision: 8,
+      },
     },
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
-    }
+      ignore: [/web\/static\/vendor/],
+    },
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": [
-        "web/static/js/app"
-      ]
-    }
+      'js/app.js': [
+        'web/static/js/app',
+      ],
+    },
   },
 
   npm: {
     enabled: true,
     // Whitelist the npm deps to be pulled in as front-end assets.
     // All other deps in package.json will be excluded from the bundle.
-    whitelist: ["phoenix", "phoenix_html", "jquery", "bootstrap", "tether"],
+    whitelist: ['phoenix', 'phoenix_html', 'jquery', 'bootstrap', 'tether'],
     globals: {
       $: 'jquery',
       jQuery: 'jquery',
-      Tether: 'tether'
-    }
-  }
+      Tether: 'tether',
+    },
+  },
 };
