62369fdd3d5fcda3c17708886a15c232f62d0144
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-26 23:38:58 +0100

    feat(role_checker): add test

---
+++ test/models/role_checker_test.exs
@@ -0,0 +1,17 @@
+defmodule Lyn.RoleCheckerTest do
+  use Lyn.ModelCase
+  alias Lyn.RoleChecker
+  alias Lyn.Factory
+
+  test "is_admin? is true when user has an admin role" do
+    role = Factory.create(:role, admin: true)
+    user = Factory.create(:user, email: "test@test.com", username: "user", password: "test", password_confirmation: "test", role: role)
+    assert RoleChecker.is_admin?(user)
+  end
+
+  test "is_admin? is false when user does not have an admin role" do
+    role = Factory.create(:role, admin: false)
+    user = Factory.create(:user, email: "test@test.com", username: "user", password: "test", password_confirmation: "test", role: role)
+    refute RoleChecker.is_admin?(user)
+  end
+end
