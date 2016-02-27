e1b716adadd78a917a69a0f40f2590bba82f0647
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-27 01:33:34 +0100

    Add crossdomain.xml

--- lib/lyn/endpoint.ex
+++ lib/lyn/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Lyn.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :lyn, gzip: false,
-    only: ~w(css fonts images js favicon.ico humans.txt robots.txt)
+    only: ~w(css fonts images js crossdomain.xml favicon.ico humans.txt robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
