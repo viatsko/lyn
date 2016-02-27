d40ad98c1996cce441dec7fc41bdb9345e495673
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-27 01:25:57 +0100

    Add font-awesome to project

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,8 @@ module.exports = {
 
   module: {
     loaders: [
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -42,8 +44,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("css!sass?outputStyle=expanded&sourceMap&includePaths[]="
-                  + (path.resolve(__dirname + "/node_modules/bootstrap/scss")))
+        loader: ExtractTextPlugin.extract("css!sass?outputStyle=expanded&sourceMap")
       },
     ],
   },
