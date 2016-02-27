57c39ee29c808271c0ee5105e43e173b93397310
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-27 15:40:44 +0100

    feat(admin): sidebars

--- web/helpers/link_helper.ex
+++ web/helpers/link_helper.ex
@@ -1,4 +1,18 @@
 defmodule Lyn.LinkHelper do
+  use Application
+
+  def sidebar(conn) do
+    path = Enum.join(Enum.drop(conn.path_info, 1), "_") <> ".html"
+
+    eex_path = Path.join([Application.app_dir(:lyn), "../../../../web/templates/admin/sidebar", path <> ".eex"])
+
+    if File.exists?(eex_path) do
+      path
+    else
+      "default.html"
+    end
+  end
+
   def active_class(conn, path) do
     current_path = Path.join(["/" | conn.path_info])
     if path == current_path do
---
+++ web/views/admin/sidebar_view.ex
@@ -0,0 +1,3 @@
+defmodule Lyn.Admin.SidebarView do
+  use Lyn.Web, :view
+end
