1db62eb7448a52f8f081c2a7d0a0ad6e79a9974f
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-27 02:42:13 +0100

    Merging in admin routes

--- web/router.ex
+++ web/router.ex
@@ -16,25 +16,33 @@ defmodule Lyn.Router do
   scope "/", Lyn do
     pipe_through :browser # Use the default browser stack
 
+    # Static
     get "/", PageController, :index
 
-    get "/admin", AdminController, :index
-
+    # Authentication
     resources "/users", UserController
-
     resources "/sessions", SessionController, only: [:new, :create, :delete]
 
+    # Admin Routes
+    get "/admin", AdminController, :index
+
     resources "/admin/objects", Admin.ObjectController
-
     resources "/admin/object_types", Admin.ObjectTypeController
-
     resources "/admin/languages", Admin.LanguageController
-
     resources "/admin/sites", Admin.SiteController
-
     resources "/admin/domains", Admin.DomainController
-
     resources "/admin/users", Admin.UserController
+
+    get "/admin/:resource/", AdminController, :index
+    get "/admin/:resource/new", AdminController, :new
+    get "/admin/:resource/csv", AdminController, :csv
+    get "/admin/:resource/:id", AdminController, :show
+    get "/admin/:resource/:id/edit", AdminController, :edit
+    post "/admin/:resource/", AdminController, :create
+    patch "/admin/:resource/:id", AdminController, :update
+    put "/admin/:resource/:id", AdminController, :update
+    delete "/admin/:resource/:id", AdminController, :destroy
+    post "/admin/:resource/batch_action", AdminController, :batch_action
   end
 
   # Other scopes may use custom stacks.
