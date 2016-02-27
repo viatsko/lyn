691b68287152d26578d5ce54671505906f113865
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-27 01:28:12 +0100

    style(webpack.config.js): bit of style

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
