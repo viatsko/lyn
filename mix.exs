7590c54bb89394419f007f66c127a292169f8051
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-28 10:26:47 +0100

    :rocket: Bump version up to 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Lyn.Mixfile do
 
   def project do
     [app: :lyn,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
