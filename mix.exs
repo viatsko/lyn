52be635866f43b35083465cf3e41e3bcbb7791f6
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-02-29 23:19:48 +0100

    :rocket: elixir@1.2.0+ dep

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lyn.Mixfile do
   def project do
     [app: :lyn,
      version: "0.0.7",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
