7a4745996ec304de48519bb77d0c60d37407d474
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-02 23:40:04 +0100

    feat(router): add guardian to api upfront

--- web/router.ex
+++ web/router.ex
@@ -11,6 +11,8 @@ defmodule Lyn.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
   end
 
   pipeline :session_auth do
