8a3971cc2cc0cfb208003e71e0f210116dc193cb
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-03 00:44:37 +0100

    feat(user): update controller

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,98 +1,11 @@
 defmodule Lyn.UserController do
   use Lyn.Web, :controller
 
+  alias Lyn.Repo
   alias Lyn.User
-  alias Lyn.Role
+  alias Lyn.Authorization
 
-  plug :scrub_params, "user" when action in [:create, :update]
-  plug :authorize_admin when action in [:new, :create]
-  plug :authorize_user when action in [:edit, :update, :delete]
-
-  def index(conn, _params) do
-    users = Repo.all(User)
-    render(conn, "index.html", users: users)
-  end
-
-  def new(conn, _params) do
-    roles = Repo.all(Role)
-    changeset = User.changeset(%User{})
-    render(conn, "new.html", changeset: changeset, roles: roles)
-  end
-
-  def create(conn, %{"user" => user_params}) do
-    roles = Repo.all(Role)
-    changeset = User.changeset(%User{}, user_params)
-
-    case Repo.insert(changeset) do
-      {:ok, _user} ->
-        conn
-        |> put_flash(:info, "User created successfully.")
-        |> redirect(to: user_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset, roles: roles)
-    end
-  end
-
-  def show(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-    render(conn, "show.html", user: user)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    roles = Repo.all(Role)
-    user = Repo.get!(User, id)
-    changeset = User.changeset(user)
-    render(conn, "edit.html", user: user, changeset: changeset, roles: roles)
-  end
-
-  def update(conn, %{"id" => id, "user" => user_params}) do
-    roles = Repo.all(Role)
-    user = Repo.get!(User, id)
-    changeset = User.changeset(user, user_params)
-
-    case Repo.update(changeset) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "User updated successfully.")
-        |> redirect(to: user_path(conn, :show, user))
-      {:error, changeset} ->
-        render(conn, "edit.html", user: user, changeset: changeset, roles: roles)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(user)
-
-    conn
-    |> put_flash(:info, "User deleted successfully.")
-    |> redirect(to: user_path(conn, :index))
-  end
-
-  defp authorize_user(conn, _) do
-    user = get_session(conn, :current_user)
-    if user && (Integer.to_string(user.id) == conn.params["id"] || Lyn.RoleChecker.is_admin?(user)) do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You are not authorized to modify that user!")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
-
-  defp authorize_admin(conn, _) do
-    user = get_session(conn, :current_user)
-    if user && Lyn.RoleChecker.is_admin?(user) do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You are not authorized to create new users!")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
+  def new(conn, params, current_user, _claims) do
+    render conn, "new.html", current_user: current_user
   end
 end
