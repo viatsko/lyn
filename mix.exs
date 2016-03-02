5206edf70cc6563e29b11cdeacfeee327230632b
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-02 22:35:40 +0100

    Add ueberauth_identity to dependencies

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Lyn.Mixfile do
                     :ueberauth,
                     :ueberauth_facebook,
                     :ueberauth_github,
+                    :ueberauth_identity,
                     :ueberauth_twitter]]
   end
 
@@ -61,6 +62,7 @@ defmodule Lyn.Mixfile do
      {:ueberauth, "~> 0.2"},
      {:ueberauth_facebook, "~> 0.3.2"},
      {:ueberauth_github, "~> 0.2"},
+     {:ueberauth_identity, "~> 0.2.2"},
      {:ueberauth_twitter, "~> 0.2.2"}]
   end
 
