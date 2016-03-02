9f13869d43d3b102765e7fc645f55635bb61ae9c
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-02 23:30:16 +0100

    feat(user_from_auth): update to work with User model

--- web/models/user_from_auth.ex
+++ web/models/user_from_auth.ex
@@ -1,24 +1,20 @@
 defmodule UserFromAuth do
-  @moduledoc """
-  Retrieve the user information from an auth request
-  """
-  
   alias Ueberauth.Auth
 
+  alias Ueberauth.Auth.Credentials
+
+  alias Lyn.User
+
   def find_or_create(%Auth{provider: :identity} = auth) do
-    case validate_pass(auth.credentials) do
+    case validate_password(auth.credentials) do
       :ok ->
-        {:ok, basic_info(auth)}
-      {:error, reason} -> {:error, reason}
+        {:ok, %User{id: auth.uid, username: name_from_auth(auth), avatar: auth.info.image}}
+      { :error, reason } -> {:error, reason}
     end
   end
 
   def find_or_create(%Auth{} = auth) do
-    {:ok, basic_info(auth)}
-  end
-
-  defp basic_info(auth) do
-    %{id: auth.uid, name: name_from_auth(auth), avatar: auth.info.image}
+    {:ok, %User{id: auth.uid, username: name_from_auth(auth), avatar: auth.info.image}}
   end
 
   defp name_from_auth(auth) do
@@ -27,22 +23,12 @@ defmodule UserFromAuth do
     else
       name = [auth.info.first_name, auth.info.last_name]
       |> Enum.filter(&(&1 != nil and &1 != ""))
-
-      cond do
-        length(name) == 0 -> auth.info.nickname
-        true -> Enum.join(name, " ")
-      end
+      if length(name) == 0, do: auth.info.nickname, else: name = Enum.join(name, " ")
     end
   end
 
-  defp validate_pass(%{other: %{password: ""}}) do
-    {:error, "Password required"}
-  end
-  defp validate_pass(%{other: %{password: pw, password_confirmation: pw}}) do
-    :ok
-  end
-  defp validate_pass(%{other: %{password: _}}) do
-    {:error, "Passwords do not match"}
-  end
-  defp validate_pass(_), do: {:error, "Password Required"}
+  defp validate_password(%Credentials{other: %{password: ""}}), do: {:error, "Password required"}
+  defp validate_password(%Credentials{other: %{password: pw, password_confirmation: pw}}), do: :ok
+  defp validate_password(%Credentials{other: %{password: _}}), do: { :error, "Passwords do not match" }
+  defp validate_password(_), do: {:error, "Password Required"}
 end
