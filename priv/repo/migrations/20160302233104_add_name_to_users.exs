778a33268cffca24f6bbb1fc653124c45d1f2228
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-03 00:31:47 +0100

    Add name to users, auth helpers

---
+++ priv/repo/migrations/20160302233104_add_name_to_users.exs
@@ -0,0 +1,9 @@
+defmodule Lyn.Repo.Migrations.AddNameToUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :name, :string
+    end
+  end
+end
---
+++ web/auth/guardian_serializer.ex
@@ -0,0 +1,13 @@
+defmodule Lyn.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias Lyn.Repo
+  alias Lyn.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+
+end
