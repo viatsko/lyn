0409478d75ef81bdc12466234a292408f90a963b
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-03 08:47:18 +0100

    Remake FE as for PhoenixGuardian

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Lyn.PageController do
   use Lyn.Web, :controller
 
   def index(conn, _params, current_user, _claims) do
-    render conn, "index.html"
+    render conn, "index.html", current_user: current_user
   end
 end
