7e40edb692dba346f8d9e52cdb128c4b6e6dc941
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-05 13:20:04 +0100

    feat(fe): fixed menu

--- web/containers/App/App.js
+++ web/containers/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { IndexLink, Link } from 'react-router';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,20 +12,20 @@ export default class App extends React.Component {
         <nav className="navbar navbar-static-top navbar-dark bg-inverse">
           <a className="navbar-brand" href="#">Project name</a>
           <ul className="nav navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" to={'/'}>
+            <li className="nav-item">
+              <IndexLink className="nav-link" activeClassName="active" to={'/'}>
                 Home <span className="sr-only">(current)</span>
-              </Link>
+              </IndexLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/about'}>About</Link>
+              <Link className="nav-link" activeClassName="active" to={'/about'}>About</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to={'/news'}>News</Link>
+              <Link className="nav-link" activeClassName="active" to={'/news'}>News</Link>
             </li>
           </ul>
         </nav>
-        
+
         {this.props.children}
 
         <hr />
