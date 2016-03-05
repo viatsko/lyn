e002f70bea1bac153844c96147630682f99056e5
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-05 13:44:19 +0100

    feat(page): cleaned up controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -16,7 +16,6 @@ defmodule Lyn.PageController do
     <%= WebPack.header %>
     <title>Lyn App</title>
     <link rel="stylesheet" href="/css/app.css">
-    <style><%= render.css %></style>
   </head>
   <body>
     <div id="content"><%= render.html %></div>
@@ -27,12 +26,10 @@ defmodule Lyn.PageController do
   """
 
   defp send_rendered_resp(conn, component) do
-    #render = Reaxt.render!(:my_router, conn.request_path)
     render = Reaxt.render!(component, %{
       location: conn.request_path,
       items: ["Home","Contact","Pages"]
     })
-    #render = Reaxt.render!(:App, conn.request_path)
 
     conn
     |> put_resp_content_type("text/html")
@@ -43,11 +40,8 @@ defmodule Lyn.PageController do
     send_rendered_resp(conn, :index)
 
   def file(conn,params, _current_user, _claims) do
-    # %{conn|path_info: ["webpack","static",params["filename"]]}
     conn
     |> put_resp_content_type("text/javascript")
     |> text(File.read!(Path.join([:code.priv_dir(:lyn), "static", params["filename"]])))
-    #text(conn, "hello #{params["filename"]}")  #%{conn|path_info: ["static",params["filename"]]}
-    #send_file(conn, Path.join(:code.priv_dir("static"), params["filename"]))
   end
 end
