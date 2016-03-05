7b3913d7240f2baa5e0f6292550c8b44c48f192d
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-05 11:16:39 +0100

    style(components/app.js): eslintify

--- web/components/app.js
+++ web/components/app.js
@@ -1,38 +1,45 @@
-var React = require("react");
-var CssHeader = require("./multi_components.js").with_css
-var config = require("reaxt/config")
+import React from 'react';
+import {with_css as CssHeader} from './multi_components'; // eslint-disable-line
+import config from 'reaxt/config';
 
 module.exports = class app extends React.Component {
+  static propTypes = {
+    items: React.PropTypes.array
+  };
+
   constructor(props) {
     super(props);
 
     this.state = {
       focused: 0
     };
-  }
 
-  getInitialState() {
-    return { focused: 0 }
+    this._clicked = (index) => this.clicked.bind(this, index);
   }
 
   clicked(index) {
-    this.setState({focused: index})
+    this.setState({ focused: index });
   }
 
   render() {
-    var self = this
+    const self = this;
+
     return (
       <div>
-        <CssHeader></CssHeader>
+        <CssHeader />
         <div>Example config : { config.example_config }</div>
-        <ul>{ this.props.items.map(function(m, index){
-          var style = ''
-          if(self.state.focused == index) style = 'focused'
-          return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>
-        }) }
+        <ul>{this.props.items.map((m, index) => {
+          let style = '';
+
+          if (self.state.focused === index) {
+            style = 'focused';
+          }
+
+          return <li className={style} onClick={this._clicked(index)}>{m}</li>;
+        })}
         </ul>
         <p>Selected: {this.props.items[this.state.focused]}</p>
       </div>
-    )
+    );
   }
-}
+};
