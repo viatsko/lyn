a62e88cae4881007bce3fe20f2758fe3375a8347
Valerii Iatsko <valerii.iatsko@icloud.com>
2016-03-05 13:16:08 +0100

    Stylize App.js

--- web/containers/App/App.js
+++ web/containers/App/App.js
@@ -9,18 +9,29 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>
-            <Link to={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>About</Link>
-          </li>
-          <li>
-            <Link to={'/news'}>News</Link>
-          </li>
-        </ul>
+        <nav className="navbar navbar-static-top navbar-dark bg-inverse">
+          <a className="navbar-brand" href="#">Project name</a>
+          <ul className="nav navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to={'/'}>
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={'/about'}>About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={'/news'}>News</Link>
+            </li>
+          </ul>
+        </nav>
+        
         {this.props.children}
+
+        <hr />
+        <footer>
+          <p>&copy; Company 2015</p>
+        </footer>
       </div>
     );
   }
